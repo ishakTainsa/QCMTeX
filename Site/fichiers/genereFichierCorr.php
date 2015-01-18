9651f47587e1fcef236237e00821832a85d8a7f0
Tauul <christopherbleschet@gmail.com>
2015-01-18 14:03:14 +0100

    test pour regler les problemes

--- Site/fichiers/genereFichierCorr.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-header('Content-Type : text');
-header('Content-Disposition : attachement; filename="fichierCorr.corr"');
-$fichier = $_SESSION['corrige'];
-echo $fichier;
-?>
--- Site/fichiers/pied.php
+++
@@ -1,9 +0,0 @@
-					</div>
-				</div>
-			</div>	
-		</section>
-		<footer>
-			<p><span id="pied_page">QCMTeX - Projet de S3 du groupe E17. <a target="_blank" href="https://github.com/Tauul/QCMTeX">Voir GitHub</a></span></p>
-		</footer>
-	</body>
-</html>
